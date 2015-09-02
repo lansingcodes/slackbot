97ff687e63f337fb780db6b805cc1d2616a096eb
Chris Fritz <chrisvfritz@gmail.com>
2015-09-02 16:40:49 -0400

    added special room for tweeting from twitter room

---
+++ twitter-config.js
@@ -0,0 +1,13 @@
+{
+  "accounts": {
+    "lansingcodes": {
+      "consumer_key": process.env.TWITTER_LANSINGCODES_CONSUMER_KEY,
+      "consumer_secret": process.env.TWITTER_LANSINGCODES_CONSUMER_SECRET,
+      "access_token": process.env.TWITTER_LANSINGCODES_ACCESS_TOKEN,
+      "access_token_secret": process.env.TWITTER_LANSINGCODES_ACCESS_TOKEN_SECRET
+    }
+  },
+  "rooms": {
+    "twitter": "lansingcodes"
+  }
+}
