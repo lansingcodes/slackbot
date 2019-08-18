e5435a8b21c4834a2a989ea84a223538e9226ff5
Erik Gillespie <erik.gillespie@gmail.com>
2019-08-17 20:14:00 -0400

    Remove old Docker configs

--- deploy/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd $HOME
-
-sudo $(aws ecr get-login --region us-east-1)
-sudo docker-compose pull lubot
-sudo docker-compose stop lubot
-sudo docker-compose rm -f lubot
-sudo docker-compose up -d
