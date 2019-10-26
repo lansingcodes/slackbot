8c5b7af2b6b1474c40d9cc5b15d7fe177bbd9385
lucidmachine <lucid.machine@gmail.com>
2019-10-26 17:22:37 -0400

    Convert new-participant-notifier-spec.ls to JavaScript.

---
+++ spec/lib/initializers/new-participant-notifier-spec.js
@@ -0,0 +1,22 @@
+describe('new-participant-notifier', () => {
+  includeHubot();
+
+  beforeEach(() => {
+    require('../../../lib/initializers/new-participant-notifier')(robot);
+  });
+
+  it('notifies "chrisvfritz" when a new user enters the "general" room', (done) => {
+    const newUser = robot.brain.userForId('2', {
+      name: 'new-user',
+      room: 'general'
+    });
+
+    robot.adapter.on('send', (envelope, strings) => {
+      expect(envelope.room).toEqual('@chrisvfritz');
+      expect(strings[0]).toEqual('new-user just joined general - just giving you a heads up so they can receive a warm welcome :-)');
+      done();
+    });
+
+    hubotHelpers.receiveEntrance(newUser);
+  });
+});
