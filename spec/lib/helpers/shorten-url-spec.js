1f98ddff28ee740c9ece6e19b04d06946412d92e
lucidmachine <lucid.machine@gmail.com>
2019-10-27 00:05:28 -0400

    Convert shorten-url-spec.ls to JavaScript.

---
+++ spec/lib/helpers/shorten-url-spec.js
@@ -0,0 +1,18 @@
+const shortenUrl = require('../../../lib/helpers/shorten-url');
+
+describe('shorten-url', () => {
+  let shortUrl;
+
+  describe('when shortening "http://www.google.com/"', () => {
+    beforeEach((done) => {
+      shortenUrl('http://www.google.com/', (url) => {
+        shortUrl = url;
+        done();
+      });
+    });
+
+    it('should return a URL beginning with "http://goo.gl/"', () => {
+      expect(shortUrl.match('http://tinyurl.com/')).toBeTruthy();
+    });
+  });
+});
