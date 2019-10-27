c4a3d12c6e1b1c70c3d0529d230010433b9930db
lucidmachine <lucid.machine@gmail.com>
2019-10-27 00:30:20 -0400

    Convert include-hubot.ls to JavaScript.

---
+++ spec/helpers/include-hubot.js
@@ -0,0 +1,37 @@
+const Robot = require('hubot/src/robot');
+const TextMessage = require('hubot/src/message').TextMessage;
+const EnterMessage = require('hubot/src/message').EnterMessage;
+
+module.exports = () => {
+  beforeEach((done) => {
+    // Create new robot, without http, using the mock adapter
+    global.robot = new Robot(null, 'mock-adapter', false, 'slackbot');
+
+    // When the robot is connected
+    robot.adapter.on('connected', () => {
+      // Create a user
+      const user = robot.brain.userForId('1', {
+        name: 'jasmine',
+        room: 'jasmine'
+      });
+
+      // Define helper methods for receiving messages
+      global.hubotHelpers = {
+        receiveMessage: (message) => {
+          robot.adapter.receive(new TextMessage(user, message));
+        },
+        receiveEntrance: (newUser) => {
+          robot.adapter.receive(new EnterMessage(newUser));
+        }
+      };
+
+      done();
+    });
+
+    robot.run();
+  });
+
+  afterEach(() => {
+    robot.shutdown();
+  });
+};
