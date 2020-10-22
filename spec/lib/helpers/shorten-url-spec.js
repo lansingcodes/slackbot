7719c1b5f3ad7a6f65aea5accaecedb8b4476396
Erik Gillespie <erik.gillespie@gmail.com>
2020-10-22 19:19:14 -0400

    Update Jasmine version and config, fix test

--- spec/lib/helpers/shorten-url-spec.js
+++ spec/lib/helpers/shorten-url-spec.js
@@ -1,18 +1,10 @@
 const shortenUrl = require('../../../lib/helpers/shorten-url')
 
 describe('shorten-url', () => {
-  let shortUrl
-
-  describe('when shortening "http://www.google.com/"', () => {
-    beforeEach((done) => {
-      shortenUrl('http://www.google.com/', (url) => {
-        shortUrl = url
-        done()
-      })
-    })
-
-    it('should return a URL beginning with "http://goo.gl/"', () => {
-      expect(shortUrl.match('http://tinyurl.com/')).toBeTruthy()
+  it('produces a URL beginning with "https://tinyurl.com/"', (done) => {
+    shortenUrl('http://www.google.com', (shortUrl) => {
+      expect(shortUrl.startsWith('https://tinyurl.com/')).toBeTrue()
+      done()
     })
   })
 })
