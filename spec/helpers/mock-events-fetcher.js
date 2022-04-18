6f9fd2e9bfe00f3c495a7582fc75ec4ed6121f31
Erik Gillespie <erik.gillespie@gmail.com>
2022-04-17 22:45:22 -0400

    Upgrade to Firebase 9 package

--- spec/helpers/mock-events-fetcher.js
+++
@@ -1,5 +0,0 @@
-module.exports = (upcomingEvents) => {
-  return (robot) => Promise.resolve({
-    upcoming: () => Promise.resolve(upcomingEvents)
-  })
-}
--- spec/helpers/mock-firestore.js
+++
@@ -1,8 +0,0 @@
-module.exports = fakeSnapshot => {
-  const firestore = {}
-  firestore.collection = () => firestore
-  firestore.where = () => firestore
-  firestore.orderBy = () => firestore
-  firestore.get = () => Promise.resolve(fakeSnapshot)
-  return Promise.resolve(firestore)
-}
