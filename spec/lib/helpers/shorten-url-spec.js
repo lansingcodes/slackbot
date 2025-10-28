01324160ad11cbe0e660068e8f6cff376404d0ca
rhamenator <rhamenator@gmail.com>
2025-10-28 00:00:54 -0400

    Refactor URL shortening logic to ensure HTTPS and improve error handling

--- spec/lib/helpers/shorten-url-spec.js
+++ spec/lib/helpers/shorten-url-spec.js
@@ -1,6 +1,17 @@
+const TinyURL = require('tinyurl')
 const shortenUrl = require('../../../lib/helpers/shorten-url')
 
 describe('shorten-url', () => {
+  beforeEach(() => {
+    spyOn(TinyURL, 'shorten').and.callFake((longUrl, cb) => {
+      const encoded = Buffer.from(longUrl)
+        .toString('base64')
+        .replace(/[+/=]/g, '')
+        .slice(0, 8)
+      cb(`https://tinyurl.com/${encoded}`)
+    })
+  })
+
   it('produces a URL beginning with "https://tinyurl.com/"', (done) => {
     shortenUrl('http://www.google.com', (shortUrl) => {
       expect(shortUrl.startsWith('https://tinyurl.com/')).toBeTrue()
